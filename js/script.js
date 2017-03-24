ad5a75013edc72192641810ac53681367cb0ce5f
Server\Admin <den58rus@mail.ru>
2017-03-24 15:40:47 +0300

    cv
    ccv

--- js/script.js
+++ js/script.js
@@ -1,30 +1,7 @@
-    /*<script>
-        var chart = AmCharts.makeChart( "chartdiv", {
-            "type": "serial",
-            "dataLoader": {
-            "url": "testConBD.php"
-    },
-    "pathToImages": "http://www.amcharts.com/lib/images/",
-    "categoryField": "data",
-    "dataDateFormat": "YYYY-MM-DD",
-    "startDuration": 1,
-
-    "categoryAxis": {
-      "parseDates": true
-    },
-
-    "graphs": [ {
-      "valueField": "summa",
-      "bullet": "round",
-      "bulletBorderColor": "#FFFFFF",
-      "bulletBorderThickness": 2,
-      "lineThickness ": 2,
-      "lineAlpha": 0.5
-    } ]
-      } );
-    </script>
-*/
+    
     // Во-первых, давайте загрузим библиотеку jQuery и запустим функцию, когда документ уже готов (когда DOM уже загружен)
+    
+    
     $(document).ready(function() {
 
         var hash = window.location.hash.substr(1);
@@ -35,53 +12,22 @@
                 $('#content').load(toLoad)
             }
         });
-
+        // ** Загрузка МЕНЮ**//
         $(function () {
                 $('#menu').load('./menu.html')
+            return false;
             })
 
 
-        $(function  () {
-            $('p').css('color','red');
-        });
+
 
         $.ajax({
-            url: "connect_BD.php",
-         //     type: 'POST',
-         //     data: {"Name":""},
-         //     dataType: 'json',
-         //    contentType: 'application/json',
-         //     json: true
-         // }).done(function(data){
-         //     console.log(data);
-         // }).always(function(dataError){
-         //     console.log(dataError);
-            success: function(data{ alert("<pre>"+ data+") </pre>"); }
+            type : 'POST',
+            url : 'connect_BD.php',
+            success: function(data){
+                $('#content').
+                console.log(data);
+            },
         });
 
-
-        /*$('#nav li a').click(function () {
-
-            var toLoad = $(this).attr('href') + ' #content';
-            $('#content').hide('fast', loadContent);
-            $('#load').remove();
-            $('#wrapper').append('<span id="load">LOADING...</span>');
-            $('#load').fadeIn('normal');
-            window.location.hash = $(this).attr('href').substr(0, $(this).attr('href').length - 5);
-            function loadContent() {
-                $('#content').load(toLoad, '', showNewContent())
-            }
-
-            function showNewContent() {
-                $('#content').show('normal', hideLoader());
-            }
-
-            function hideLoader() {
-                $('#load').fadeOut('normal');
-            }
-
-            return false;*/
-
-
-    });
-
+    })
